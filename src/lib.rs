1988dd433adc652d713b65415f79896bbc412681
Leiko Lei <chachafrdark@gmail.com>
2022-10-20 23:20:38 +0200

    all tests passing

--- src/lib.rs
+++ src/lib.rs
@@ -7,15 +7,15 @@ struct Parser<T> {
     run: Run<T>
 }
 
-type Run<T> = Arc<dyn Fn(ParserInput) -> (ParserInput, Result<T, ParserError>)>;
+type Run<T> = Arc<dyn Fn(ParserInput) -> (ParserInput, Result<T, String>)>;
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 struct ParserInput {
     text: String,
     pos: usize
 }
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, PartialEq)]
 struct ParserError {
     desc: String,
     pos: usize
@@ -28,7 +28,7 @@ fn input_sub (start: usize, len: usize, s: &ParserInput) -> ParserInput {
     }
 }
 
-fn fail<T> (e: ParserError) -> Parser<T> {
+fn fail<T> (e: String) -> Parser<T> {
     Parser {
         run: Arc::new(move |input| {
             let e = e.clone();
@@ -85,10 +85,7 @@ fn bind<A: 'static, B: 'static> (f: Box<dyn Fn(A) -> Parser<B>>, p: Parser<A>) -
 fn prefix (prefix_str: &'static str) -> Parser<&str> {
     Parser {
         run: Arc::new(move |input| {
-            let unexpected_prefix_error = ParserError {
-                desc: format!("expected {}", prefix_str).to_string(),
-                pos: 0
-            };
+            let unexpected_prefix_error = format!("expected {}", prefix_str).to_string();
 
             let prefix_size = prefix_str.len();
             let input_size = input.text.len();
@@ -99,6 +96,77 @@ fn prefix (prefix_str: &'static str) -> Parser<&str> {
                 (rest, Ok(prefix_str))
             } else {
                 (input, Err(unexpected_prefix_error))
+            }
+        })
+    }
+}
+
+fn optional<A: 'static> (p: Parser<A>) -> Parser<Option<A>> {
+    Parser {
+        run: Arc::new(move |input| {
+            let (input_, result) = (p.run)(input);
+            match result {
+                Ok(x) => (input_, Ok(Some(x))),
+                Err(_) => (input_, Ok(None))
+            }
+        })
+    }
+}
+
+fn many_exact<A: 'static> (n: i32, p: Parser<A>) -> Parser<Vec<A>> {
+    Parser {
+        run: Arc::new(move |input| {
+            let mut xs = Vec::new();
+            let mut input_ = input;
+            for _ in 0..n {
+                let (input__, result) = (p.run)(input_);
+                match result {
+                    Ok(x) => {
+                        xs.push(x);
+                        input_ = input__;
+                    },
+                    Err(e) => {
+                        return (input__, Err(e));
+                    }
+                }
+            }
+            (input_, Ok(xs))
+        })
+    }
+}
+
+fn many<A: 'static> (p: Parser<A>) -> Parser<Vec<A>> {
+    Parser {
+        run: Arc::new(move |input| {
+            let mut xs = Vec::new();
+            let mut input_ = input;
+            loop {
+                let (input__, result) = (p.run)(input_);
+                input_ = input__;
+                match result {
+                    Ok(x) => {
+                        xs.push(x);
+                    },
+                    Err(_) => {
+                        break;
+                    }
+                }
+            }
+            (input_, Ok(xs))
+        })
+    }
+}
+
+fn any_char () -> Parser<char> {
+    Parser {
+        run: Arc::new(|input| {
+            let n = input.text.len();
+            if n >= 1 {
+                (input_sub(1, n-1, &input), Ok(input.text.as_bytes()[0] as char))
+            } else {
+                let empty_input_error = format!("expected any char, got none (input.len() = {n}").to_string();
+
+                (input, Err(empty_input_error))
             }
         })
     }
@@ -170,6 +238,22 @@ impl<B: 'static, A: 'static> ops::Add<Parser<B>> for Parser<A> {
     }
 }
 
+impl<A: 'static> ops::BitOr<Parser<A>> for Parser<A> {
+    type Output = Parser<A>;
+
+    fn bitor(self, p2: Parser<A>) -> Self::Output {
+        Parser {
+            run: Arc::new(move |input| {
+                let (input_, result) = (self.run)(input.clone());
+                match result {
+                    Ok(x) => (input_, Ok(x)),
+                    Err(_) => (p2.run)(input)
+                }
+            })
+        }
+    }
+}
+
 fn make_input (s: String) -> ParserInput {
     ParserInput {
         text: s,
@@ -177,19 +261,103 @@ fn make_input (s: String) -> ParserInput {
     }
 }
 
+fn run<A> (p: Parser<A>, input: String) -> Result<A, ParserError> {
+    match (p.run)(make_input(input)) {
+        (_, Ok(x)) => Ok(x),
+        (input, Err(desc)) => Err(ParserError {
+            desc,
+            pos: input.pos
+        })
+    }
+}
+
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let input = make_input("key1 = value1".to_string());
+    fn key_value_pair_parser_test() {
+        let input = "key1 = value1".to_string();
         let wss = parse_while(Box::new(|x| x.is_whitespace()));
 
         let name_parser = parse_while(Box::new(|x| x.is_alphanumeric()));
         let entry_parser = (wss.clone() >> name_parser.clone() << wss.clone() << prefix("=")) + (wss.clone() >> name_parser.clone());
 
-        let output = (entry_parser.run)(input);
-        dbg!(output);
+        let parsed = run(entry_parser, input);
+        assert_eq!(parsed, Ok(("key1".to_string(), "value1".to_string())));
+    }
+
+    #[test]
+    fn or_test() {
+        let input = "111aaa".to_string();
+        let parser = prefix("aaa") | prefix("111");
+
+        let parsed = run(parser, input);
+        assert_eq!(parsed, Ok("111"));
+    }
+
+    #[test]
+    fn optional_test() {
+        // test with working input
+        let input = "111aaa".to_string();
+        let parser = optional(prefix("111"));
+
+        let parsed = run(parser, input);
+        assert_eq!(parsed, Ok(Some("111")));
+
+        // test with no valid input to parse
+        let input = "aaa".to_string();
+        let parser = optional(prefix("111"));
+
+        let parsed = run(parser, input);
+        assert_eq!(parsed, Ok(None));
+    }
+
+    #[test]
+    fn any_char_test() {
+        // test with working input
+        let parser = any_char();
+        let parsed = run(parser, "hello".to_string());
+        assert_eq!(parsed, Ok('h'));
+
+        // test with empty input, should fail
+        let parser = any_char();
+        let parsed = run(parser, "".to_string());
+
+        assert_eq!(parsed, Err(ParserError{
+            desc: format!("expected any char, got none (input.len() = {}", 0).to_string(),
+            pos: 0
+        }));
+    }
+
+    #[test]
+    fn many_exact_test() {
+        // test with input.len() = 3 (so parser succeeds)
+        let input = "hel".to_string();
+        let parser = many_exact(3, any_char());
+
+        let parsed = run(parser, input);
+        assert_eq!(parsed, Ok(vec!['h', 'e', 'l']));
+
+        // test with input.len() < 3 (so parser fails)
+        let input = "he".to_string();
+        let parser = many_exact(3, any_char());
+
+        let parsed = run(parser, input);
+        assert_eq!(parsed, Err(ParserError{
+            desc: format!("expected any char, got none (input.len() = {}", 0).to_string(),
+            pos: 2
+        }));
+    }
+
+    #[test]
+    fn many_test() {
+        // test with working input
+        let input = "hello".to_string();
+        let parser = many(any_char());
+
+        let parsed = run(parser, input);
+        assert_eq!(parsed, Ok(vec!['h', 'e', 'l', 'l', 'o']));
     }
 }
